8a3bcedc9dc38b6adcb411b259752c9733c8e601
weissenborn24seb@gmail.com <weissenborn24seb@gmail.com>
2023-11-28 09:41:08 +0100

    uplaod

--- server/app.js
+++ server/app.js
@@ -4,23 +4,24 @@ const app = express();
 require("./db/conn");
 const router = require("./routes/router");
 const cors = require("cors");
-
+const path = require("path"); // Import the 'path' module
 
 const port = 8005;
 
-
-app.use(express.json({limit:"50mb", extended: true}));
-// app.use(express.json());
-app.use(cors())
+app.use(express.json({ limit: "50mb", extended: true }));
+app.use(cors());
 app.use(router);
 
-app.use("/uploads",express.static("./uploads"));
-app.use("/videos",express.static("./videos"));
+app.use("/uploads", express.static("./uploads"));
+app.use("/videos", express.static("./videos"));
 
+// Serve static files from the current directory
+// app.use(express.static(path.join(__dirname, 'index.html')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
-
-app.listen(port,()=>{
-    console.log(`server start at port no ${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
